fa2f651dc4e10fc4ccf3880ec7ddae2336ae34df
Josiel Castillo <josielcastillov@gmail.com>
2023-10-06 10:02:10 -0600

    Agregar archivos de proyecto.

---
+++ POOUI/Persona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOUI
+{
+    //Clase o Plantilla
+    public class Persona
+    {
+        //Caracteristicas o Atributos
+        public string Cedula;
+        public string Nombres;
+        public string Apellidos;
+        public int Edad;
+        public double Peso;
+        public bool EsEstudiante;
+
+        //Constructor por Defecto
+        public Persona()
+        {
+
+        }
+
+        //Contrustor con parametros
+        public Persona(string Cedula, string Nombres, string Apellidos, int Edad, double Peso, bool EsEstudiante)
+        {
+            this.Cedula = Cedula;
+            this.Nombres = Nombres;
+            this.Apellidos = Apellidos;
+            this.Edad = Edad;
+            this.Peso = Peso;
+            this.EsEstudiante = EsEstudiante;
+        }
+
+        //Comportamientos o Metodos
+        public void Estudiar()
+        {
+            Console.WriteLine("La Persona {0} esta Estudiando", Nombres);
+        }
+    }
+}
---
+++ POOUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace POOUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intancion de un objeto con el contructor por defecto
+            Persona persona = new Persona();
+
+            persona.Cedula = "001-200798100V";
+            persona.Nombres = "Josiel Antonio";
+            persona.Apellidos = "Castillo Valle";
+            persona.Edad = 20;
+            persona.Peso = 120.5;
+            persona.EsEstudiante = true;
+
+            //Intancion de un objeto con el contructor por parametros
+            Persona persona1 = new Persona("002-146845-456123", "Pedro Pablo", "Ruiz Peralta", 50, 150, false);
+
+
+            Console.WriteLine(persona.Nombres);
+            persona.Estudiar();
+
+            Console.WriteLine(persona1.Nombres);
+            persona1.Estudiar();
+
+            Console.ReadKey();
+        }
+    }
+}
